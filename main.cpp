52066ff2ce3afa07540bebaf8c81f9f40864e09f
“Todor <lucatodor54@gmail.com>
2026-08-04 10:39:43 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//todo
+// Instructiunea de afisare
+//  cout<<"test"; //afiseaza pe ecran ce este in ghilimele
+//  cout<<"test2"<<endl; imi pune un rand liber dupa test2
+//
+void ex1Afisare() {
+
+    cout<<"ce mai faci";//afiseaza pe ecran cuvantul ce mai faci
+   // cout<<endl;
+    cout<<"test"<<endl;
+    cout<<"ana are mere";
+}
+
+int main() {
+
+
+    ex1Afisare();
+
+}
